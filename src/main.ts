d184e9a84905e75bf44d0face9d0455fe6839a92
William Dutra <will.dutra26@gmail.com>
2021-11-03 22:02:03 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('Olá');
+import Samsung from './Samsung';
+import Command from './Models/Command';
+import { KEYS } from './Constantes/KEYS';
+import { CMD } from './Constantes/CMD';
+
+
+
+
+(async() => {
+ 
+	let tv = new Samsung('localhost', '8001', 'App Name');
+	let token = await tv.getTokenId();
+	await tv.sendCommand(KEYS.KEY_VOLUP, CMD.CLICK, token);
+
+	console.log('finished');
+ 
+ 
+})()
+
+
+
