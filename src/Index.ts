8b4735d894034dda51dbe07a85e0b943a8055917
William Dutra <will.dutra26@gmail.com>
2021-11-07 17:53:07 -0300

    remocao do webpack

---
+++ src/Index.ts
@@ -0,0 +1,30 @@
+import Samsung from './Samsung';
+// import Command from './Models/Command';
+// import { KEYS } from './Constantes/KEYS';
+// import { CMD } from './Constantes/CMD';
+
+
+
+
+// (async() => {
+ 
+	// let tv = new Samsung('192.168.15.3', '8001', 'App Name');
+	// let token = await tv.getTokenId();
+	
+	// // await tv.sendCommand(KEYS.KEY_1, CMD.CLICK, token);
+	// // await tv.sendCommand(KEYS.KEY_3, CMD.CLICK, token);
+	// // await tv.sendCommand(KEYS.KEY_ENTER, CMD.CLICK, token);
+	
+	// //await tv.getAppsInstaled(token);
+	
+	// //await tv.openAppsById('111299001912', token);
+	// let img = await tv.getAppIconByPath('/opt/share/webappservice/apps_icon/FirstScreen/3201601007230/250x250.png', token);
+	// console.log(img);
+
+	// console.log('finished');
+ 
+ 
+// })()
+
+
+export default Samsung;
